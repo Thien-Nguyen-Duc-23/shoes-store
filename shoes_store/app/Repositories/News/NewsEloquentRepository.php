56502269f4f078cdcadd23e982063e4cf9052ec7
Thien-Nguyen-Duc-23 <tducnguyen1997@gmail.com>
2020-10-26 14:15:18 +0700

    Shose-25 implement moedel news

--- shoes_store/app/Repositories/News/NewsEloquentRepository.php
+++ shoes_store/app/Repositories/News/NewsEloquentRepository.php
@@ -11,4 +11,32 @@ class NewsEloquentRepository extends EloquentRepository implements NewsRepositor
     {
         return News::class;
     }
+
+    public function getThreeNew($limit)
+    {
+        return $this->model::orderBy('created_at', 'desc')->limit($limit)->get();
+    }
+
+    public function getNews()
+    {
+        $idThreeNew = $this->getThreeNew(3)->pluck('id')->toArray();
+
+        return $this->model::whereNotIn('id', $idThreeNew)
+            ->orderBy('created_at', 'desc')
+            ->get();
+    }
+
+    public function findNewsBySlug($slug)
+    {
+        return $this->model::where('slug', $slug)
+            ->first();
+    }
+
+    public function getRelateNews($id, $limit)
+    {
+        return $this->model::where('id', '<>', $id)
+            ->orderBy('created_at', 'desc')
+            ->limit($limit)
+            ->get();
+    }
 }
